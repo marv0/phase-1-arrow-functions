716031251e3279a5467e51812929abb07683c0be
Marvin Ogot <marvin.ogot@gmail.com>
2023-10-07 20:00:52 +0300

    Completed Tasks

---
+++ index.js
@@ -0,0 +1,10 @@
+//divide function expression that divides 2000 by 100
+const divide = function(){
+return 2000 / 100;
+};
+
+//arrow function called square that takes a parameter and multiplies it by itslef
+const square = num1 => num1 * num1;
+
+//arrow function called add that takes two parameters and adds them together
+const add = (num2,num3) => num2 + num3;
